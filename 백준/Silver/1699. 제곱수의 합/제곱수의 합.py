fc89bb09bbf6725c5e2dfe04de0aca78f16e2e8b
Kyeonghwan Kim <65859079+kyeong8@users.noreply.github.com>
2023-10-05 17:25:28 +0900

    [Silver II] Title: 제곱수의 합, Time: 6740 ms, Memory: 31620 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.py
@@ -0,0 +1,13 @@
+import sys
+N = int(sys.stdin.readline())
+
+dp = [0] * 100001
+
+for i in range(1, N + 1):
+    dp[i] = i
+    for j in range(1, i):
+        if (j * j) > i:
+            break
+        if dp[i] > dp[i - j * j] + 1:
+            dp[i] = dp[i - j * j] + 1
+print(dp[N])
