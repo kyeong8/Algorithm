e73b34f36848c8a30bc0aafade7f6cffa8f9f061
Kyeonghwan Kim <65859079+kyeong8@users.noreply.github.com>
2023-10-06 06:34:51 +0900

    [Silver I] Title: 카드 구매하기, Time: 256 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -0,0 +1,12 @@
+import sys
+#sys.stdin = open('input.txt', 'r')
+
+N = int(sys.stdin.readline().rstrip())
+pack = [0]
+pack.extend(list(map(int, sys.stdin.readline().split())))
+
+for i in range(1, len(pack)):
+    for j in range(1, i):
+        pack[i] = max(pack[i], pack[i-j] + pack[j])
+
+print(pack[N])
