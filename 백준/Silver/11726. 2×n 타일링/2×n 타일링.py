61652f96848ba162256f395ee45e15a6baca5c3b
Kyeonghwan Kim <65859079+kyeong8@users.noreply.github.com>
2023-10-06 06:37:39 +0900

    [Silver III] Title: 2×n 타일링, Time: 320 ms, Memory: 31308 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,18 @@
+import sys
+sys.setrecursionlimit(10**7)
+#sys.stdin = open('input.txt', 'r')
+
+def factorial(num):
+    if num == 0:
+        return 1
+    if num == 1:
+        return 1
+    return num * factorial(num - 1)
+
+
+n = int(sys.stdin.readline().rstrip())
+cnt = 1
+for i in range(1, n//2 + 1):
+    temp = factorial(n-i) // (factorial(n - 2*i) * factorial(i))
+    cnt += temp
+print(cnt%10007)
