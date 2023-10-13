6e84971ed8eafec1435a64c5332060ec5cd03cc3
Kyeonghwan Kim <65859079+kyeong8@users.noreply.github.com>
2023-10-13 20:41:39 +0900

    [Gold III] Title: 사다리 조작, Time: 348 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.cc
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.cc
@@ -24,11 +24,7 @@ bool check()
 
 		while (true)
 		{
-			//cout << "row: " << row << " col: " << col << "\n";
-			/*if (j == 4 && arr[1][3] && arr[4][2] && arr[6][4])
-			{
-				cout << "[dibug]1 " << "row: " << row << " col: " << col << "\n";
-			}*/
+			// 가로선은 왼쪽에서 오른쪽으로 향함. 즉, 왼쪽 기준
 			if (arr[row][col - 1] == 1)
 				--col;
 			else if (arr[row][col] == 1)
@@ -41,19 +37,14 @@ bool check()
 		}
 
 		if (j != col)
-		{
-			//cout << "j: " << j << " col: " << col << "\n";
 			return false;
-		}
-			
 	}
 
 	return true;
 }
 
-void combination(int depth, int type)
+void combination(int depth, int type, int idx)
 {
-	//cout << "depth: " << depth << " type: " << type << "\n";
 	if (type == depth)
 	{
 		if (check())
@@ -61,15 +52,15 @@ void combination(int depth, int type)
 		return;
 	}
 
-	for (auto i : v)
+	for (int i = idx; i < v.size(); i++)
 	{
-		Pos temp = i;
+		Pos temp = v[i];
 
 		if (arr[temp.row][temp.col] != 1 && arr[temp.row][temp.col - 1] != 1 && arr[temp.row][temp.col + 1] != 1)
 		{
 			arr[temp.row][temp.col] = 1;
 
-			combination(depth + 1, type);
+			combination(depth + 1, type, i);
 
 			arr[temp.row][temp.col] = 0;
 		}
@@ -84,13 +75,11 @@ int main()
 
 	//freopen("input.txt", "r", stdin);
 
-
 	int a, b;
 
 	// H -> 가로 -> row, N -> 세로 -> col
 	cin >> N >> M >> H;
 
-	//cout << "N: " << N << " M: " << M << " H: " << H << "\n";
 	for (int i = 0; i < M; i++)
 	{
 		cin >> a >> b;
@@ -101,34 +90,20 @@ int main()
 	{
 		for (int j = 1; j < N; j++)
 		{
+			// 가로선을 놓을 수 있는 후보
+			// 왼쪽 column, 현재 column, 오른쪽 column 모두 가로선이 없어야 놓을 수 있음
 			if (arr[i][j] != 1 && arr[i][j + 1] != 1 && arr[i][j - 1] != 1)
 				v.push_back(Pos{ i, j });
-
-			//if (arr[i][j] != 1 && arr[i][j + 1] != 1 && j < N)
-			//	v.push_back(make_pair(Pos{ i, j }, Pos{ i, j + 1}));
 		}
 	}
 
-	//for (auto i : v)
-	//	cout << i.row << " " << i.col << "\n";
-
-	//arr[1][3] = 1;
-
-	//arr[4][2] = 1;
-
-	//arr[6][4] = 1;
-
-	//cout << "check: " << check() << "\n";
-
 	for (int i = 0; i <= 3; i++)
 	{
-		//cout << "type: " << i << "\n";
-		//cout << "size: " << v.size() << "\n";
-		combination(0, i);
+		combination(0, i, 0);
 		if (minNum != 4)
 			break;
 	}
-		
+
 	if (minNum == 4)
 		minNum = -1;
 	cout << minNum << "\n";
