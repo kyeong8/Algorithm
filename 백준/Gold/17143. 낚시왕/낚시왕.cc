e50bb7867dd4e351fdd74327a462c96425d75d41
Kyeonghwan Kim <65859079+kyeong8@users.noreply.github.com>
2023-10-14 01:27:50 +0900

    [Gold I] Title: 낚시왕, Time: 32 ms, Memory: 2144 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.cc
+++ 백준/Gold/17143. 낚시왕/낚시왕.cc
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <map>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
@@ -20,7 +21,7 @@ struct Shark {
 // 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18
 
 int R, C, M;
-bool arr[102][102] = { false };
+Shark arr[102][102];
 int hunter = 0;
 int answer = 0;
 
@@ -48,22 +49,28 @@ int main()
 
 	//freopen("input.txt", "r", stdin);
 
-	map<pair<int, int>, Shark> shark;
-	
 	cin >> R >> C >> M;
+
+	for (int i = 1; i <= R; i++)
+	{
+		for (int j = 1; j <= C; j++)
+		{
+			arr[i][j] = { 0, 0, 0 };
+		}
+	}
+
 	int r, c, s, d, z;
 	for (int i = 0; i < M; i++)
 	{
 		cin >> r >> c >> s >> d >> z;
-		shark[make_pair( r, c )] = Shark{ s, d, z };
-		arr[r][c] = true;
+		arr[r][c] = { s, d, z };
 	}
 
 	//for (int i = 1; i <= R; i++)
 	//{
 	//	for (int j = 1; j <= C; j++)
 	//	{
-	//		cout << arr[i][j] << " ";
+	//		cout << arr[i][j].d << " ";
 	//	}
 	//	cout << "\n";
 	//}
@@ -83,134 +90,146 @@ int main()
 		for (int i = 1; i <= R; i++)
 		{
 			// 격자판에서 잡은 상어가 사라진다.
-			if (arr[i][hunter])
+			if (arr[i][hunter].z != 0)
 			{
-				answer += shark[make_pair(i, hunter)].z;
-				shark.erase(make_pair(i, hunter));
-				arr[i][hunter] = false;
-				break; 
+				answer += arr[i][hunter].z;
+				arr[i][hunter] = { 0, 0, 0 };
+				break;
 			}
 		}
 
 		// 상어가 이동한다.
-		map<pair<int, int>, Shark> mem;
-		for (auto sh : shark)
+		Shark mem[102][102];
+		for (int i = 1; i <= R; i++)
 		{
-			int row = sh.first.first;
-			int col = sh.first.second;
-			int direct = sh.second.d;
-			int speed = sh.second.s;
-			int size = sh.second.z;
-
-			//cout << "row: " << row << " col: " << col << "\n";
-
-			arr[row][col] = false;
-			int nrow, ncol;
-			// 왼쪽 오른쪽
-			if (direct > 2)
+			for (int j = 1; j <= C; j++)
 			{
-				nrow = row, ncol = col;
-				while (true)
-				{
-					//cout << "Speed: " << speed << "\n";
-					//cout << "ncol: " << ncol << "\n";
-					//cout << "direct: " << direct << "\n";
-					
-					if (direct == 4 && speed > ncol - 1)
-					{
-						speed -= ncol - 1;
-						direct = change(direct);
-						ncol = 1;
-					}
-					else if (direct == 3 && speed > C - ncol)
-					{
-						speed -= C - ncol;
-						direct = change(direct);
-						ncol = C;
-					}
-					else if (direct == 4 && speed <= ncol - 1)
-					{
-						ncol -= speed;
-						speed = 0;
-					}
-					else if (direct == 3 && speed <= C - ncol)
-					{
-						ncol += speed;
-						speed = 0;
-					}
-
-					if (speed == 0)
-						break;
-				}
-				//cout << "destination: " << ncol << "\n\n";
+				mem[i][j] = { 0, 0, 0 };
 			}
-			// 위 아래
-			else if (direct <= 2)
-			{
-				nrow = row, ncol = col;
-				while (true)
-				{
-					//cout << "Speed: " << speed << "\n";
-					//cout << "ncol: " << ncol << "\n";
-					//cout << "direct: " << direct << "\n";
-
-					if (direct == 1 && speed > nrow - 1)
-					{
-						speed -= nrow - 1;
-						direct = change(direct);
-						nrow = 1;
-					}
-					else if (direct == 2 && speed > R - nrow)
-					{
-						speed -= R - nrow;
-						direct = change(direct);
-						nrow = R;
-					}
-					else if (direct == 1 && speed <= nrow - 1)
-					{
-						nrow -= speed;
-						speed = 0;
-					}
-					else if (direct == 2 && speed <= R - nrow)
-					{
-						nrow += speed;
-						speed = 0;
-					}
-
-					if (speed == 0)
-						break;
-				}
-				//cout << "destination: " << ncol << "\n\n";
-			}
-
-			if (mem.find(make_pair(nrow, ncol)) != mem.end())
-			{
-				int compareSize = mem[make_pair(nrow, ncol)].z;
-
-				if (size > compareSize)
-					mem[make_pair(nrow, ncol)] = Shark{ sh.second.s, direct, sh.second.z };
-			}
-			else
-				mem[make_pair(nrow, ncol)] = Shark{ sh.second.s, direct, sh.second.z };
 		}
-		shark = mem;
 
-		for (auto sh : shark)
-			arr[sh.first.first][sh.first.second] = true;
+		for (int i = 1; i <= R; i++)
+		{
+			for (int j = 1; j <= C; j++)
+			{
+				if (arr[i][j].z == 0)
+					continue;
+
+				int row = i;
+				int col = j;
+				int direct = arr[i][j].d;
+				int speed = arr[i][j].s;
+				int size = arr[i][j].z;
+
+
+				int nrow, ncol;
+				// 왼쪽 오른쪽
+				if (direct > 2)
+				{
+					nrow = row, ncol = col;
+					while (true)
+					{
+						//cout << "Speed: " << speed << "\n";
+						//cout << "ncol: " << ncol << "\n";
+						//cout << "direct: " << direct << "\n";
+
+						if (direct == 4 && speed > ncol - 1)
+						{
+							speed -= ncol - 1;
+							direct = change(direct);
+							ncol = 1;
+						}
+						else if (direct == 3 && speed > C - ncol)
+						{
+							speed -= C - ncol;
+							direct = change(direct);
+							ncol = C;
+						}
+						else if (direct == 4 && speed <= ncol - 1)
+						{
+							ncol -= speed;
+							speed = 0;
+						}
+						else if (direct == 3 && speed <= C - ncol)
+						{
+							ncol += speed;
+							speed = 0;
+						}
+
+						if (speed == 0)
+							break;
+					}
+					//cout << "destination: " << ncol << "\n\n";
+				}
+				// 위 아래
+				else if (direct <= 2)
+				{
+					nrow = row, ncol = col;
+					while (true)
+					{
+						//cout << "Speed: " << speed << "\n";
+						//cout << "ncol: " << ncol << "\n";
+						//cout << "direct: " << direct << "\n";
+
+						if (direct == 1 && speed > nrow - 1)
+						{
+							speed -= nrow - 1;
+							direct = change(direct);
+							nrow = 1;
+						}
+						else if (direct == 2 && speed > R - nrow)
+						{
+							speed -= R - nrow;
+							direct = change(direct);
+							nrow = R;
+						}
+						else if (direct == 1 && speed <= nrow - 1)
+						{
+							nrow -= speed;
+							speed = 0;
+						}
+						else if (direct == 2 && speed <= R - nrow)
+						{
+							nrow += speed;
+							speed = 0;
+						}
+
+						if (speed == 0)
+							break;
+					}
+					//cout << "destination: " << ncol << "\n\n";
+				}
+
+				if (mem[nrow][ncol].z != 0)
+				{
+					int compareSize = mem[nrow][ncol].z;
+
+					if (size > compareSize)
+						mem[nrow][ncol] = Shark{ arr[i][j].s, direct, size };
+				}
+				else
+					mem[nrow][ncol] = Shark{ arr[i][j].s, direct, size };
+			}
+		}
+
+		for (int i = 1; i <= R; i++)
+		{
+			for (int j = 1; j <= C; j++)
+			{
+				arr[i][j] = mem[i][j];
+			}
+		}
 
 		//for (int i = 1; i <= R; i++)
 		//{
 		//	for (int j = 1; j <= C; j++)
 		//	{
-		//		cout << arr[i][j] << " ";
+		//		cout << arr[i][j].d << " ";
 		//	}
 		//	cout << "\n";
 		//}
 		//cout << "\n";
-
-		//for (auto sh : shark)
-		//	cout << "row: " << sh.first.first << " col: " << sh.first.second << " direct: " << sh.second.d << " speed: " << sh.second.s  << " size:" << sh.second.z << "\n";
-		//cout << "\n";
+		
 	}
 
 	cout << answer << "\n";
