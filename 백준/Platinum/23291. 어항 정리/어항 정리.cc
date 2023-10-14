bd9f0e76daa05124af2b11dcfc7a715b4da68d73
Kyeonghwan Kim <65859079+kyeong8@users.noreply.github.com>
2023-10-14 16:37:14 +0900

    [Platinum V] Title: 어항 정리, Time: 0 ms, Memory: 2072 KB -BaekjoonHub

--- 백준/Platinum/23291. 어항 정리/어항 정리.cc
+++ 백준/Platinum/23291. 어항 정리/어항 정리.cc
@@ -46,7 +46,7 @@ void bfs(int prevRow, int index)
 							{
 								mem[nrow][ncol] += value / 5;
 								mem[a][b] -= value / 5;
-							}	
+							}
 						}
 					}
 				}
@@ -76,7 +76,8 @@ int main()
 
 	//freopen("input.txt", "r", stdin);
 
-	int index;
+	// 물고기가 있는 어항 중 가장 작은 열
+	int index = 0;
 	int answer = 0;
 
 	vector<pair<int, int>> small;
@@ -119,25 +120,14 @@ int main()
 		map[0][0] = 0;
 		index = 1;
 
-		//for (int i = 5; i >= 0; i--)
-		//{
-		//	for (int j = 0; j < N; j++)
-		//	{
-		//		cout << map[i][j] << " ";
-		//	}
-		//	cout << "\n";
-		//}
-		//cout << "\n";
-
 		// 2개 이상 쌓여있는 어항을 모두 공중 부양시킨 다음, 전체를 시계방향으로 90도 회전시킨다
-
 		int row;
 		int col;
 
 		while (true)
 		{
-			row = 0;
-			col = 0;
+			row = 0; // 공중 부양 해야하는 행의 개수
+			col = 0; // 공중 부양 해야하는 열의 개수
 
 			for (int i = index; i < N; i++)
 			{
@@ -145,8 +135,6 @@ int main()
 					++col;
 				else
 					break;
-				//cout << "map[1][i]: " << map[1][i] << "\n";
-				//cout << "col: " << col << "\n";
 			}
 
 			for (int i = 0; i < N; i++)
@@ -157,46 +145,30 @@ int main()
 					break;
 			}
 
+			// index = 3, row = 3, col = 2
+			// 공중 부양시키고 회전시켜 쌓았을 때 범위를 넘는 경우
 			if (row + index + col > N)
 				break;
 
-			//cout << "row: " << row << " col: " << col << "\n\n";
-			// (0, 2), (0, 3), (1, 2), (1, 3) -> 
 			for (int i = 0; i < row; i++)
 			{
 				for (int j = index; j < col + index; j++)
 				{
-					map[col + index - j][index + i + col] = map[i][j];
+					// 쌓였을 때 높이 index는 공중 부양하는 column 개수가 된다.
+					// 거기서 column의 변화량 j - index 만큼 빠져 높이가 결정됨
+					// 오른쪽으로 증가하면 아래로 감소하기 때문
+					map[col - (j - index)][index + i + col] = map[i][j];
 					map[i][j] = 0;
 				}
 			}
 
-			//for (int i = 5; i >= 0; i--)
-			//{
-			//	for (int j = 0; j < N; j++)
-			//	{
-			//		cout << map[i][j] << " ";
-			//	}
-			//	cout << "\n";
-			//}
-			//cout << "\n";
-
+			// 공중 부양되어 쌓인 어항으로 인해
+			// 가장 작은 2층이상 어항의 index를 재갱신
 			index += col;
 		}
 
 		bfs(row, index);
 
-		//for (int i = 5; i >= 0; i--)
-		//{
-		//	for (int j = 0; j < N; j++)
-		//	{
-		//		cout << map[i][j] << " ";
-		//	}
-		//	cout << "\n";
-		//}
-		//cout << "\n";
-
-		//cout << "index: " << index << " row: " << row << " col: " << col << "\n";
 		for (int i = 0; i < col; i++)
 		{
 			for (int j = 0; j < row; j++)
@@ -206,34 +178,18 @@ int main()
 			}
 		}
 
-		//for (int i = 5; i >= 0; i--)
-		//{
-		//	for (int j = 0; j < N; j++)
-		//	{
-		//		cout << map[i][j] << " ";
-		//	}
-		//	cout << "\n";
-		//}
-		//cout << "\n";
-
 		// 첫번째 접기
+		// 앞에 반절을 행을 증가시켜 역순으로 옮기기
 		for (int i = 0; i < N / 2; i++)
 		{
 			map[1][N - 1 - i] = map[0][i];
 			map[0][i] = 0;
 		}
 
-		//for (int i = 5; i >= 0; i--)
-		//{
-		//	for (int j = 0; j < N; j++)
-		//	{
-		//		cout << map[i][j] << " ";
-		//	}
-		//	cout << "\n";
-		//}
-		//cout << "\n";
 
 		// 두번째 접기
+		// 뒤에 반절부터 다시 층마다 역순으로 옮기기
+		// 가장 낮은 층이 가장 높은 층이 됨
 		for (int i = 0; i < 2; i++)
 		{
 			for (int j = 0; j < N / 4; j++)
@@ -243,40 +199,19 @@ int main()
 			}
 		}
 
-		//for (int i = 5; i >= 0; i--)
-		//{
-		//	for (int j = 0; j < N; j++)
-		//	{
-		//		cout << map[i][j] << " ";
-		//	}
-		//	cout << "\n";
-		//}
-		//cout << "\n";
-
 		bfs(4, N - N / 4 - 1);
 
-		for (int i = 0; i < N / 4; i++)
+		// 펼치기
+		for (int i = 0; i < N / 4; i++) // 열
 		{
-			for (int j = 0; j < 4; j++)
+			for (int j = 0; j < 4; j++) // 행
 			{
 				map[0][i * 4 + j] = map[j][N - N / 4 + i];
 				map[j][N - N / 4 + i] = 0;
 			}
 		}
-
-		//for (int i = 5; i >= 0; i--)
-		//{
-		//	for (int j = 0; j < N; j++)
-		//	{
-		//		cout << map[i][j] << " ";
-		//	}
-		//	cout << "\n";
-		//}
-		//cout << "\n";
-
-		//cout << "answer: " << answer << "\n\n";
 	}
-	
+
 	cout << answer << "\n";
 
 	// (0, 0), (1, 0) -> (1, 0), (1, 1)
