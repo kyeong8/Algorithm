a3312243e37b8c688e23d48cfe56b0720d52d569
Kyeonghwan Kim <65859079+kyeong8@users.noreply.github.com>
2024-10-06 02:30:51 +0900

    [Gold II] Title: 어른 상어, Time: 0 ms, Memory: 2068 KB -BaekjoonHub

--- 백준/Gold/19237. 어른 상어/어른 상어.cc
+++ 백준/Gold/19237. 어른 상어/어른 상어.cc
@@ -1,151 +1,40 @@
-// 상어 번호마다 다른 우선순위를 어떻게 다룰 것인가
-// 상어의 이동과 냄새를 어떻게 다룰 것인가
-
-#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
-struct Pos {
-	int row;
-	int col;
-};
+int N, M, K;
 
 struct Shark {
 	int row;
 	int col;
 	int direct;
-	bool del;
+	bool exit;
 };
 
 struct Smell {
 	int num;
-	int rest;
+	int remain;
 };
 
-int dr[] = { 0, -1, 1, 0, 0 };
-int dc[] = { 0, 0, 0, -1, 1 };
+int pd[401][5][5] = { 0 };
+int dr[5] = { 0, -1, 1, 0, 0 };
+int dc[5] = { 0, 0, 0, -1, 1 };
 
-int live;
-int N, M, k;
+Smell smell[20][20];
 Shark shark[401];
-int priority[401][5][4];
-int graph[21][21];
-Smell smell[21][21];
 
-bool outofrange(int row, int col)
+bool noSmell(int row, int col)
 {
-	if (1 <= row && row <= N && 1 <= col && col <= N)
-		return false;
-	return true;
+	if (0 <= row && row < N && 0 <= col && col < N && smell[row][col].remain == 0)
+		return true;
+	return false;
 }
 
-void touch()
+bool mySmell(int row, int col, int num)
 {
-	for (int i = 1; i <= M; i++)
-	{
-		if (shark[i].del)
-			continue;
-
-		smell[shark[i].row][shark[i].col] = { i, k };
-	}
-}
-
-void move()
-{
-	vector<int> mem[21][21];
-	bool flag[401] = { false };
-	for (int i = 1; i <= M; i++)
-	{
-		if (shark[i].del)
-			continue;
-
-		for (int j = 0; j < 4; j++)
-		{
-			int direct = priority[i][shark[i].direct][j];
-			int nrow = shark[i].row + dr[direct];
-			int ncol = shark[i].col + dc[direct];
-
-			if (!outofrange(nrow, ncol) && smell[nrow][ncol].rest == 0)
-			{
-				flag[i] = true;
-
-				shark[i].direct = direct;
-				shark[i].row = nrow;
-				shark[i].col = ncol;
-
-				mem[shark[i].row][shark[i].col].push_back(i);
-				
-				break;
-			}
-		}
-	}
-
-	
-	for (int i = 1; i <= M; i++)
-	{
-		if (shark[i].del || flag[i])
-			continue;
-
-		for (int j = 0; j < 4; j++)
-		{
-			int direct = priority[i][shark[i].direct][j];
-			int nrow = shark[i].row + dr[direct];
-			int ncol = shark[i].col + dc[direct];
-
-			if (!outofrange(nrow, ncol) && smell[nrow][ncol].num == i)
-			{
-				shark[i].direct = direct;
-				shark[i].row = nrow;
-				shark[i].col = ncol;
-
-				mem[shark[i].row][shark[i].col].push_back(i);
-				break;
-			}
-		}
-	}
-
-
-	for (int i = 1; i <= N; i++)
-	{
-		for (int j = 1; j <= N; j++)
-		{
-			if (mem[i][j].size() >= 2)
-			{
-				sort(mem[i][j].begin(), mem[i][j].end());
-
-				graph[shark[mem[i][j][0]].row][shark[mem[i][j][0]].col] = mem[i][j][0];
-				for (int idx = 1; idx < mem[i][j].size(); idx++)
-				{
-					shark[mem[i][j][idx]].del = true;
-					--live;
-				}
-			}
-			else if (mem[i][j].size() == 1)
-				graph[i][j] = mem[i][j][0];
-			else if (mem[i][j].empty())
-				graph[i][j] = 0;
-		}
-	}
-}
-
-void delSmell()
-{
-	for (int i = 1; i <= N; i++)
-	{
-		for (int j = 1; j <= N; j++)
-		{
-			if (smell[i][j].rest > 0)
-			{
-				--smell[i][j].rest;
-
-				if (smell[i][j].rest == 0)
-					smell[i][j].num = 0;
-			}
-		}
-	}
+	if (0 <= row && row < N && 0 <= col && col < N && smell[row][col].num == num)
+		return true;
+	return false;
 }
 
 int main()
@@ -154,95 +43,171 @@ int main()
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	//freopen("input.txt", "r", stdin);
+	// freopen("input.txt", "r", stdin);
 
-	cin >> N >> M >> k;
+	cin >> N >> M >> K;
 
-	live = M;
-	int temp;
-	for (int i = 1; i <= N; i++)
+	int arr[20][20] = { 0 };
+	for (int i = 0; i < N; i++)
 	{
-		for (int j = 1; j <= N; j++)
+		for (int j = 0; j < N; j++)
 		{
+			int temp;
 			cin >> temp;
 			if (temp > 0)
-				shark[temp] = { i, j, 0, false };
-			graph[i][j] = temp;
-			smell[i][j] = { 0, 0 };
-		}
-	}
-
-	for (int i = 1; i <= M; i++)
-	{
-		cin >> temp;
-		shark[i].direct = temp;
-	}
-
-	for (int i = 1; i <= M; i++)
-	{
-		for (int j = 1; j < 5; j++)
-		{
-			for (int k = 0; k < 4; k++)
 			{
-				cin >> temp;
-				priority[i][j][k] = temp;
+				shark[temp].row = i;
+				shark[temp].col = j;
+				shark[temp].exit = false;
+				smell[i][j].num = temp;
+				smell[i][j].remain = K;
 			}
 		}
 	}
 
-	touch();
+	for (int i = 1; i <= M; i++)
+		cin >> shark[i].direct;
 
-	int round = 0;
+	for (int i = 1; i <= M; i++)
+	{
+		for (int j = 1; j <= 4; j++)
+		{
+			for (int k = 1; k <= 4; k++)
+			{
+				cin >> pd[i][j][k];
+			}
+		}
+	}
+
+	//for (int i = 0; i < N; i++)
+	//{
+	//	for (int j = 0; j < N; j++)
+	//	{
+	//		cout << smell[i][j].num << " ";
+	//	}
+	//	cout << "\n";
+	//}
+	//cout << "\n";
+
+	//for (int i = 0; i < N; i++)
+	//{
+	//	for (int j = 0; j < N; j++)
+	//	{
+	//		cout << smell[i][j].remain << " ";
+	//	}
+	//	cout << "\n";
+	//}
+	//cout << "\n";
+
+	int cnt = 0;
+	int time = 0;
 	while (true)
 	{
-		if (round > 1000)
+		if (time > 1000)
 			break;
-
-		++round;
-
-		move();
-		
-		if (live == 1)
+		if (M - cnt == 1)
 			break;
-
-		delSmell();
-
-		touch();
-
-		/*cout << "round: " << round << "\n";
-		for (int i = 1; i <= N; i++)
+		//cout << "time: " << time + 1 << "\n";
+		// sharkMove()
+		for (int i = 1; i <= M; i++)
 		{
-			for (int j = 1; j <= N; j++)
-			{
-				cout << smell[i][j].num << " ";
-			}
-			cout << "\n";
-		}
-		cout << "\n";
+			if (shark[i].exit)
+				continue;
 
-		for (int i = 1; i <= N; i++)
-		{
-			for (int j = 1; j <= N; j++)
-			{
-				cout << smell[i][j].rest << " ";
-			}
-			cout << "\n";
-		}
-		cout << "\n";
+			int flag = false;
+			int row = shark[i].row;
+			int col = shark[i].col;
+			int direct = shark[i].direct;
 
-		for (int i = 1; i <= N; i++)
-		{
-			for (int j = 1; j <= N; j++)
+			for (int j = 1; j <= 4; j++)
 			{
-				cout << graph[i][j] << " ";
+				int ndirect = pd[i][direct][j];
+				int nrow = row + dr[ndirect];
+				int ncol = col + dc[ndirect];
+
+				if (noSmell(nrow, ncol))
+				{
+					flag = true;
+					shark[i].row = nrow;
+					shark[i].col = ncol;
+					shark[i].direct = ndirect;
+					if (arr[nrow][ncol] == 0)
+						arr[nrow][ncol] = i;
+					else
+					{
+						shark[i].exit = true;
+						cnt++;
+					}
+					break;
+				}
 			}
-			cout << "\n";
+
+			if (!flag)
+			{
+				for (int j = 1; j <= 4; j++)
+				{
+					int ndirect = pd[i][direct][j];
+					int nrow = row + dr[ndirect];
+					int ncol = col + dc[ndirect];
+
+					if (mySmell(nrow, ncol, i))
+					{
+						shark[i].row = nrow;
+						shark[i].col = ncol;
+						shark[i].direct = ndirect;
+						arr[nrow][ncol] = i;
+						break;
+					}
+				}
+			}
 		}
-		cout << "\n";*/
+		for (int i = 0; i < N; i++)
+		{
+			for (int j = 0; j < N; j++)
+			{
+				// smellDel()
+				if (smell[i][j].remain > 0)
+				{
+					--smell[i][j].remain;
+					if (smell[i][j].remain == 0)
+						smell[i][j].num = 0;
+				}
+
+				// smellAdd()
+				if (arr[i][j] != 0)
+				{
+					smell[i][j].num = arr[i][j];
+					smell[i][j].remain = K;
+					arr[i][j] = 0;
+				}
+			}
+		}
+
+		//for (int i = 0; i < N; i++)
+		//{
+		//	for (int j = 0; j < N; j++)
+		//	{
+		//		cout << smell[i][j].num << " ";
+		//	}
+		//	cout << "\n";
+		//}
+		//cout << "\n";
+
+		//for (int i = 0; i < N; i++)
+		//{
+		//	for (int j = 0; j < N; j++)
+		//	{
+		//		cout << smell[i][j].remain << " ";
+		//	}
+		//	cout << "\n";
+		//}
+		//cout << "\n";
+
+		time++;
 	}
 
-	if (round > 1000)
+	if (time > 1000)
 		cout << "-1\n";
 	else
-		cout << round << "\n";
+		cout << time << "\n";
 }
